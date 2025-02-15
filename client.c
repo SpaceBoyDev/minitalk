fb0daccb6d14e38491b4936e0c2e6f68c0163e3d
Darío <dario.mart.lop.03@gmail.com>
2025-02-15 21:03:00 +0100

    edit: progreso. Recibo signals y tal más o menos

--- client.c
+++ client.c
@@ -6,33 +6,72 @@
 /*   By: dario <dario@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 20:05:00 by dario             #+#    #+#             */
-/*   Updated: 2025/02/12 20:02:27 by dario            ###   ########.fr       */
+/*   Updated: 2025/02/15 20:28:57 by dario            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+#define BITS 8 // Tamaño de un carácter en bits
 
-bool	arg_check(char *pid)
-{
-	size_t	i;
-	if (!pid)
-		return (false);
-	i = 0;
-	while (pid[i])
-	{
-		if (!ft_isdigit(pid[i]))
-			return (false);
-		++i;
-	}
-	return (true);
+void enviar_caracter(pid_t server_pid, char c) {
+    for (int i = BITS - 1; i >= 0; i--) {
+        int bit = (c >> i) & 1;
+        kill(server_pid, bit ? SIGUSR1 : SIGUSR2);
+        usleep(10000); // Pequeña pausa para evitar pérdida de señales
+    }
 }
 
-int	main(int argc, char **argv)
-{
-	if (argc != 3)
-		error_exit("Incorrect amount of arguments");
-	if (!arg_check(argv[1]))
-		error_exit("PID should only contain digits");
-
-	return (0);
+void enviar_mensaje(pid_t server_pid, const char *mensaje) {
+    while (*mensaje) {
+        enviar_caracter(server_pid, *mensaje);
+        mensaje++;
+    }
+    enviar_caracter(server_pid, '\0'); // Enviar fin de mensaje
 }
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Uso: %s <PID_SERVIDOR> <MENSAJE>\n", argv[0]);
+        exit(1);
+    }
+
+    pid_t server_pid = atoi(argv[1]);
+    const char *mensaje = argv[2];
+
+    printf("Cliente: Enviando mensaje \"%s\" al servidor %d\n", mensaje, server_pid);
+    enviar_mensaje(server_pid, mensaje);
+
+    return 0;
+}
+
+// bool	arg_check(char *pid)
+// {
+// 	size_t	i;
+// 	if (!pid)
+// 		return (false);
+// 	i = 0;
+// 	while (pid[i])
+// 	{
+// 		if (!ft_isdigit(pid[i]))
+// 			return (false);
+// 		++i;
+// 	}
+// 	return (true);
+// }
+
+// int	main(int argc, char **argv)
+// {
+// 	if (argc != 3)
+// 		error_exit("Incorrect amount of arguments");
+// 	if (!arg_check(argv[1]))
+// 		error_exit("PID should only contain digits");
+// 	pid_t server_pid = ft_atoi(argv[1]);
+	
+// 	if (kill(server_pid, SIGUSR1) == -1)
+// 	{
+// 		error_exit("kill");
+// 	}
+// 	sleep(1);
+	
+// 	return (0);
+// }
--- server.c
+++ server.c
@@ -6,17 +6,97 @@
 /*   By: dario <dario@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 20:05:05 by dario             #+#    #+#             */
-/*   Updated: 2025/02/12 19:27:21 by dario            ###   ########.fr       */
+/*   Updated: 2025/02/15 20:30:55 by dario            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int	main()
-{
-	ft_printf("SERVER PID: %d\n", getpid());
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <malloc.h>
 
-	while(1)
-		pause();
-	return (0);
+#define BITS 8 // Tamaño de un carácter en bits
+
+char *mensaje;
+int bit_count = 0;
+char caracter_actual = 0;
+
+void manejador(int sig) {
+    // Desplazar un bit a la izquierda
+    caracter_actual <<= 1;
+
+    // Si la señal es SIGUSR1, añadimos un '1'
+    if (sig == SIGUSR1) {
+        caracter_actual |= 1;
+    }
+
+    bit_count++;
+
+    // Cuando se reciben 8 bits, se almacena el carácter
+    if (bit_count == BITS) {
+        int len = mensaje ? ft_strlen(mensaje) : 0;
+        mensaje = realloc(mensaje, len + 2); // +2 para el nuevo char y el '\0'
+        mensaje[len] = caracter_actual;
+        mensaje[len + 1] = '\0';
+
+        if (caracter_actual == '\0') { // Fin del mensaje
+            printf("Servidor: Mensaje recibido -> \"%s\"\n", mensaje);
+            free(mensaje);
+            mensaje = NULL;
+        }
+
+        // Reiniciar contadores
+        caracter_actual = 0;
+        bit_count = 0;
+    }
 }
+
+int main() {
+    struct sigaction sa;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_handler = manejador;
+    sa.sa_flags = 0;
+
+    sigaction(SIGUSR1, &sa, NULL);
+    sigaction(SIGUSR2, &sa, NULL);
+
+    printf("Servidor iniciado. PID: %d\n", getpid());
+
+    while (1) {
+        pause(); // Espera señales
+    }
+
+    return 0;
+}
+
+
+// void handler(int sig, siginfo_t *info, void *context)
+// {
+//     printf("Recibí la señal %d del proceso %d\n", sig, info->si_pid);
+    
+//     // Leer el mensaje enviado
+//     printf("Mensaje recibido: %d\n", info->si_value.sival_int);
+// }
+
+// int	main()
+// {
+// 	struct sigaction sa;
+// 	sa.sa_flags = SA_SIGINFO;
+// 	sa.sa_sigaction = handler;
+// 	sigemptyset(&sa.sa_mask);
+
+// 	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+// 	{
+// 		error_exit("sigaction error creo");
+// 	}
+// 	ft_printf("SERVER PID: %d\n", getpid());
+	
+// 	sigaction(SIGINT, &sa, NULL);
+// 	sigaction(SIGTERM, &sa, NULL);
+// 	while(1)
+// 		pause();
+// 	return (0);
+// }
